99461e669340921198ee6668b13c9e9f4e7628af
Gustavo Kurek <gustavokureksz@gmail.com>
2022-08-07 22:13:37 -0300

    Desafio concluído 100% testes

--- Desafio-Solid/src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ Desafio-Solid/src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,14 +2,20 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+  user_id: string | string[];
 }
 
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+    if (!user || !user.admin) {
+      throw new Error("User not exist");
+    }
+
+    const userList = this.usersRepository.list();
+    return userList;
   }
 }
 
